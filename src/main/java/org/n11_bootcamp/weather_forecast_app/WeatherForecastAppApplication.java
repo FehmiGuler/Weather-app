77f258b62dab28fbd14f9a51779e5450c6991052
Fehmi <fehmigulersw@gmail.com>
2024-03-09 18:50:50 +0300

    Add main application and test application

---
+++ src/main/java/org/n11_bootcamp/weather_forecast_app/WeatherForecastAppApplication.java
@@ -0,0 +1,13 @@
+package org.n11_bootcamp.weather_forecast_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherForecastAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/n11_bootcamp/weather_forecast_app/WeatherForecastAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.n11_bootcamp.weather_forecast_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
