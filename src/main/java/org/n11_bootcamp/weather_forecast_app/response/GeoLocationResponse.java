ba177126cab2f70ea4e5a2583a1a83ee171b3b36
Fehmi <fehmigulersw@gmail.com>
2024-03-09 18:51:06 +0300

    Add responses

---
+++ src/main/java/org/n11_bootcamp/weather_forecast_app/response/GeoLocationResponse.java
@@ -0,0 +1,7 @@
+package org.n11_bootcamp.weather_forecast_app.response;
+
+public record GeoLocationResponse(String name, String country, double lon, double lat) {
+
+
+}
+
---
+++ src/main/java/org/n11_bootcamp/weather_forecast_app/response/WeatherResponse.java
@@ -0,0 +1,9 @@
+package org.n11_bootcamp.weather_forecast_app.response;
+
+import java.util.List;
+
+public record WeatherResponse(String name, Object main) {
+
+
+}
+
